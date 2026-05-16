3758be0d79c3f09837d1736e37736cfb63aeca6a
Raquel Santos <39015507+RaquelMSantos@users.noreply.github.com>
2026-05-16 13:45:24 -0300

    feat(aula 02): step-01 create initial layout

---
+++ aula-02-main-thread/app/src/main/java/com/codesbyrachel/aula02_mainthread/MainThreadScreen.kt
@@ -0,0 +1,56 @@
+package com.codesbyrachel.aula02_mainthread
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MainThreadScreen(modifier: Modifier = Modifier) {
+
+    var status by remember { mutableStateOf("Aguardando...") }
+    var isLoading by remember { mutableStateOf(false) }
+
+    Column(
+        modifier =
+            modifier
+                .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = status,
+            style = MaterialTheme.typography.headlineMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 24.dp),
+        )
+
+        Button(
+            onClick = {
+                // TODO
+            },
+            enabled = !isLoading,
+        ) {
+            Text("Buscar Dados")
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+
+        if (isLoading) {
+            CircularProgressIndicator()
+        }
+    }
+}
